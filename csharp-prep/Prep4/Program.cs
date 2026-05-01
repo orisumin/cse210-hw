311c9641266ead89ea5465efd418b648245b3936
Garyeong Bae <orisumin@gmail.com>
2026-05-01 12:43:37 -0600

    finished prep 5

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>{};
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        bool typing = true;
+        do{
+        Console.WriteLine("Enter number:");
+        string input = Console.ReadLine();
+        int number = int.Parse(input);
+        if (number == 0)
+            {
+                typing = false;
+            }        
+        numbers.Add(number);
+
+        } while(typing);
+        
+        numbers.Sort();
+        int sum = 0;
+        float average = 0;
+        int max = 0;
+        int small = numbers[0];
+
+        foreach(int number in numbers)
+        {
+            sum += number;
+        }
+        average = sum / numbers.Count;
+        max = numbers[numbers.Count-1];
+        
+        foreach(int number in numbers)
+        {
+            if ()
+        }
+
+        Console.WriteLine($"The sum is:{sum}");
+        Console.WriteLine($"The average is:{average}");
+        Console.WriteLine($"The largest number is:{max}");
+        Console.WriteLine($"The smallest positive number is:{small}");
+        Console.WriteLine("The sorted list is:");
+        foreach(int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
     }
 }
--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        try{
+            DisplayWelcome();
+            string name;
+            PromptUserName(out name);
+            int number;
+            PromptUserNumber(out number);
+            int year;
+            PromtUserBirthYear(out year);
+            int squared;
+            SquareNumber(ref number, out squared);
+            DisplayResult(name, squared, year);
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Error: Cannot divide by zero.");
+            Console.WriteLine("Exception message: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error: Exception Occurred");
+        }
+        finally
+        {
+            Console.WriteLine("This block always executes, regardless of exceptions.");
+        }
+    }
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+    static void PromptUserName(out string name)
+    {
+        Console.Write("Please enter your name:");
+        name = Console.ReadLine();
+    }
+    static void PromptUserNumber (out int number)
+    {
+        Console.Write("Please enter your favorite number:");
+        string input = Console.ReadLine();
+        number = int.Parse(input);
+    }
+    static void PromtUserBirthYear(out int year)
+    {
+        Console.Write("Please enter the year you were born:");
+        string input = Console.ReadLine();
+        year = int.Parse(input);
+    }
+    static void SquareNumber (ref int number, out int squared)
+    {
+        squared = number*number;
+    }
+    static void DisplayResult (string name, int squared, int year)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squared}");
+        Console.WriteLine($"{name}, you will turn {2026-year} this year.");
     }
 }
